1e188a1dc033c2e5d09222aef797183a510a3cb1
MrSkillage <conorrabbitte.it3@gmail.com>
2021-02-15 12:15:37 +0000

    Implemented bin_print method to bit shift and print hexadecimal.

--- bit-operations.c
+++ bit-operations.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
 
+void bin_print(int i) {
+    // Number of bits in an integer
+    int j = sizeof(int) * 8;
+
+    // Temporary Variable
+    int k;
+
+    for (j--; j>= 0; j--) {
+        k = ((1 << j) & i) ? 1 : 0;
+        printf("%d", k);
+    }
+
+}
+
 int main(int argc, char *argv[]) {
     
     // Print Hello, World!
     printf("%s\n", "Hello, World again!");
     
+    int i = 241;
+    bin_print(i);
+
+    printf("\n");
+
     return 0;
 }
